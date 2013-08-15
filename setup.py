2be85e728da88e46adf5f953c31d9b1035e059a4
Simon Chapman <simon.chapman@saffrondigital.com>
2013-08-15 13:54:13 +0100

    1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 import subprocess
 
 setup(name='wafer-thin-mint',
-    version='1.0.0',
+    version='1.0.1',
     author='SD Web Services Team',
     author_email='simon.chapman@saffrondigital.com',
     packages=find_packages())
