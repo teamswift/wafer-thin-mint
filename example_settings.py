1f5b7cf7c4c2c412884e63748297a49c9c79acf9
Simon Chapman <simon.chapman@saffrondigital.com>
2013-10-09 15:07:12 +0100

    added example settings py

---
+++ example_settings.py
@@ -0,0 +1,10 @@
+WAFER_THIN_MINT = {
+    'connector_model': 'path.to.Connector',
+    'client': {
+        'host': '127.0.0.1',
+        'suffix': '?format=json',
+        'tables': {
+            'users': '/api/v1/users/',
+        }
+    }
+}
