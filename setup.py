9c904502bae59d2815fcec695de617cd0d4679d8
Simon Chapman <simon.chapman@saffrondigital.com>
2013-10-22 14:20:03 +0100

    updated version to 1.0.11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 import subprocess
 
 setup(name='wafer-thin-mint',
-    version='1.0.10',
+    version='1.0.11',
     author='Simon Chapman - Team Swift',
     author_email='simon.chapman@saffrondigital.com',
     packages=find_packages())
